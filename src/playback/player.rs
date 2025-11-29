30b496b606618f2b49468be0285525b1ac040a8e
mengh04 <mengh0412@hotmail.com>
2025-11-29 22:40:08 +0800

    流式加载并播放声音

--- src/playback/player.rs
+++ src/playback/player.rs
@@ -1,5 +1,9 @@
+use std::path::Path;
 use std::sync::mpsc::Receiver;
 
+use kira::sound::FromFileError;
+use kira::sound::streaming::{StreamingSoundData, StreamingSoundHandle};
+use kira::{AudioManager, AudioManagerSettings, DefaultBackend, Tween};
 use tokio::sync::broadcast;
 
 use crate::playback::commands::PlayerCommand;
@@ -9,13 +13,19 @@ use crate::playback::state::PlayerState;
 pub struct Player {
     event_sender: broadcast::Sender<PlayerEvent>,
     state: PlayerState,
+    manager: AudioManager<DefaultBackend>,
+    streaming_sound_handle: Option<StreamingSoundHandle<FromFileError>>,
 }
 
 impl Player {
     pub fn new(event_sender: broadcast::Sender<PlayerEvent>) -> Self {
+        let manager = AudioManager::<DefaultBackend>::new(AudioManagerSettings::default()).unwrap();
+
         Player {
             event_sender,
             state: PlayerState::Stopped,
+            manager,
+            streaming_sound_handle: None,
         }
     }
 
@@ -25,6 +35,7 @@ impl Player {
                 PlayerCommand::Play(path) => {
                     eprintln!("playing: {}", path.display());
                     self.state = PlayerState::Playing;
+                    self.play(path);
                     let _ = self
                         .event_sender
                         .send(PlayerEvent::StateChanged(self.state.clone()));
@@ -58,5 +69,15 @@ impl Player {
                 }
             }
         }
+    }
+
+    fn play(&mut self, path: impl AsRef<Path>) {
+        if let Some(handle) = &mut self.streaming_sound_handle {
+            let _ = handle.stop(Tween::default());
+        }
+
+        let path = path.as_ref();
+        let sound_data = StreamingSoundData::from_file(path).unwrap();
+        self.streaming_sound_handle = Some(self.manager.play(sound_data).unwrap());
     }
 }
